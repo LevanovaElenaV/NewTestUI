7ac81e9bad324b9ab2afe88c6490a2359f5afafb
LevanovaElenaV <enalevster@gmail.com>
2022-08-09 02:02:35 +0300

    Выполнено задание к уроку 5.

--- src/test/java/lesson5/AfishaTest.java
+++ src/test/java/lesson5/AfishaTest.java
@@ -1,3 +1,4 @@
+// урок 5 для Git
 package lesson5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -8,13 +9,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.stream.Stream;
+//import java.util.stream.Stream;
 
 public class AfishaTest {
     WebDriver driver;
